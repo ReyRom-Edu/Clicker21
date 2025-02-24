2ddd6de9ea67108147576e7a920c32eb84717053
Roman Sadovskiy <s03roman@mail.ru>
2025-02-24 20:36:31 +0300

    подготовка

--- app/src/main/java/com/example/clicker21/MainActivity.kt
+++ app/src/main/java/com/example/clicker21/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,18 +31,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Typeface
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.clicker21.ui.theme.Clicker21Theme
+import androidx.core.content.res.ResourcesCompat
+import com.example.compose.AppTheme
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlin.math.PI
@@ -67,11 +79,11 @@ fun ClickerGame() {
     var isPressed by remember { mutableStateOf(false) }
 
     val scale by animateFloatAsState(
-        targetValue = if(isPressed) 1.1f else 1f,
-        animationSpec = tween(delayMillis = 500)
+        targetValue = if(isPressed) 0.9f else 1f,
+        animationSpec = tween(delayMillis = 10)
     )
 
-    Clicker21Theme {
+    AppTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Box(modifier = Modifier
                 .padding(innerPadding)
@@ -83,29 +95,52 @@ fun ClickerGame() {
                     modifier = Modifier.align(Alignment.TopCenter))
 
                 Box(Modifier
-                    .size(100.dp)
-                    .graphicsLayer(scaleX = scale, scaleY = scale)
+                    .size(300.dp)
                     .align(Alignment.Center)
-                    .background(Color.Blue)
+                    .clip(shape = CircleShape)
+                    .background(Color.Transparent)
                     .onGloballyPositioned {
                         boxPosition = Offset(it.positionInParent().x, it.positionInParent().y)
                     }
                     .pointerInput(Unit){
                         coroutineScope {
                             while (true){
-
                                 awaitPointerEventScope {
-                                    position = awaitFirstDown().position
+                                    val down = awaitFirstDown()
+                                    position = down.position
                                     clicks++
-                                    repeat(10){
+                                    isPressed = true
+                                    repeat(5){
                                         particles.add(Particle(position.x + boxPosition.x
                                             ,position.y + boxPosition.y))
+                                    }
+
+                                    down.consume()
+                                    val up = waitForUpOrCancellation()
+
+                                    if (up != null) {
+                                        isPressed = false
                                     }
                                 }
                             }
                         }
                     }
-                )
+                ){
+                    Image(
+                        painter = painterResource(id = R.drawable.cthulhu_star),
+                        modifier = Modifier.fillMaxSize(),
+                        contentDescription = "Background Image",
+                        contentScale = ContentScale.Crop
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.cthulhu),
+                        modifier = Modifier.fillMaxSize(0.7f)
+                            .align(Alignment.Center)
+                            .graphicsLayer(scaleX = scale, scaleY = scale),
+                        contentDescription = "Cthulhu",
+                        contentScale = ContentScale.Crop
+                    )
+                }
 
                 ParticleAnimation(particles)
             }
@@ -123,14 +158,26 @@ fun ParticleAnimation(particles: MutableList<Particle>){
             indalidate = !indalidate
         }
     }
-
+    val context = LocalContext.current
     Canvas(modifier = Modifier.fillMaxSize()) {
         indalidate.let {
             particles.forEach{ particle ->
-                drawIntoCanvas {
-                    drawCircle(Color.Red.copy(alpha = particle.alpha),
-                        radius = 8f,
-                        center = Offset(particle.x, particle.y))
+                drawIntoCanvas { canvas ->
+                    //drawCircle(Color.Red.copy(alpha = particle.alpha),
+                    //    radius = 8f,
+                    //    center = Offset(particle.x, particle.y))
+
+                    val paint = android.graphics.Paint().apply {
+                        color = android.graphics.Color.argb(particle.alpha, 0.38f,0.96f,0.86f)
+                        textSize = 80f // Размер шрифта
+                        alpha = (particle.alpha * 255).toInt()
+                        typeface = ResourcesCompat.getFont(context, R.font.daedra)
+                    }
+                    canvas.nativeCanvas.drawText(
+                        particle.letter, // Буква частицы
+                        particle.x, particle.y,
+                        paint
+                    )
                 }
             }
         }
@@ -140,9 +187,10 @@ fun ParticleAnimation(particles: MutableList<Particle>){
 
 data class Particle(var x:Float, var y:Float,
                     var alpha: Float = 1f,
-                    var rotation: Float = Random.nextFloat() * 360){
+                    var rotation: Float = Random.nextFloat() * 360,
+                    val letter: String =  ('A'..'Z').random().toString()){
     private val angle = Random.nextFloat() * 2 * PI.toFloat()
-    private val speed = Random.nextFloat() * 5 + 2
+    private val speed = Random.nextFloat() * 4 + 1
     private val speedX = speed * cos(angle)
     private val speedY = speed * sin(angle)
     private var lifeTime = 1f
@@ -150,9 +198,9 @@ data class Particle(var x:Float, var y:Float,
     fun update() : Boolean{
         x += speedX
         y += speedY
-        alpha -= 0.05f
+        alpha -= 0.02f
         rotation += 5
-        lifeTime -= 0.05f
+        lifeTime -= 0.02f
         return lifeTime > 0
     }
 }
